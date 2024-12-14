1f2a078daa32cd80ea796794a537feec6507129f
Nugraha Yudhasyah <nugrahayudhasyah@sttcipasung.ac.id>
2024-12-14 20:29:00 +0700

    feat: slicing html

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <p>&copy; 2024 STT Cipasung. Semua hak cipta dilindungi.</p>
+    </div>
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,19 @@
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="storage/images/Logo.png" alt="STT Cipasung">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">Akademik</a></li>
+                <li><a href="curriculum.html">Profile</a></li>
+                <li><a href="contact.html">Penghargaan</a></li>
+                <li><a href="layanan.html">layanan</a></li>
+                <li><a href="fasilitas.html">Fasilitas</a></li>
+                <li><a href="PMB online.html">PMB Online</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
